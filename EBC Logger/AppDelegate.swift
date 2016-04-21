a5d86ec70d1e20f9fb2d907a1040cab6a819b6cc
Zach Massia <zmassia@gmail.com>
2016-04-20 21:53:58 -0400

    Initial Commit

---
+++ EBC Logger/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  EBC Logger
+//
+//  Created by Zachary Massia on 2016-04-20.
+//  Copyright © 2016 Zachary Massia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
